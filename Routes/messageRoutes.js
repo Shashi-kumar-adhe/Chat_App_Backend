6c018908ac2350669f39e8b7e7746596271005f5
Shashi Kumar <adheshashikumar@gmail.com>
2023-10-31 22:50:28 +0530

    version3

--- Routes/messageRoutes.js
+++ Routes/messageRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {
   allMessages,
   sendMessage,
-} = require("../controllers/messageControllers");
+} = require('../Controllers/messageControllers.js');
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
